61de0b3abf9297461277675da2bd08855c69ccde
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2026-04-20 12:17:37 -0300

    feat: nav bar w/ lateral menu for mobile

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -9,5 +9,13 @@ import { RouterLink } from '@angular/router';
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
