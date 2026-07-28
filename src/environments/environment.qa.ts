1ae96402ff79519d847681129b203a7ba836b56c
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2026-07-28 19:25:02 -0300

    fix: correct backend QA URL in environment variables

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://fitoherb-backend-qa-nkrtrtj5hq-uc.a.run.app',
-  imagesBaseUrl: 'https://fitoherb-backend-qa-nkrtrtj5hq-uc.a.run.app',
+  apiUrl: 'https://fitoherb-backend-qa-973619276831.us-central1.run.app',
+  imagesBaseUrl: 'https://fitoherb-backend-qa-973619276831.us-central1.run.app',
   contactRecipient: 'danielmarinho8@hotmail.com'
 };
